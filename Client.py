392f0ee49df2421a6649df159f809287dca05e02
Pracheth2007 <60105879+Pracheth2007@users.noreply.github.com>
2022-05-17 19:19:19 +0530

    Update Client.py

--- Client.py
+++ Client.py
@@ -4,14 +4,12 @@ from tkinter import *
 from tkinter import ttk
 import ftplib
 import os
-import ntpath #This is used to extract filename from path
+import ntpath 
 import time
 from ftplib import FTP
 from tkinter import filedialog
 from pathlib import Path
 
-#import winsound
-#Uncomment the beow two line to use pygame to allow mp3 files as well
 from playsound import playsound
 import pygame
 from pygame import mixer
@@ -116,11 +114,6 @@ def download():
     else:
        infoLabel.configure(text="") 
    
-    
-
-    
-        
-#Client GUI
 def musicWindow(): 
     global song_counter
     global filePathLabel
@@ -180,8 +173,7 @@ def setup():
     SERVER.connect((IP_ADDRESS, PORT))
 
     musicWindow()
-
-#Initiate Server Connection    
+  
 setup()
 
 
